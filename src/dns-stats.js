9e94e1982d6d6d506828398a507619eb3f2c3de9
unknown <home_dv@mail.ru>
2023-04-17 18:04:47 +0300

    feat: complete dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let res = {};
+  domains.forEach(d => {
+    let buff = [];
+    let str = '';
+    buff = d.split('.').reverse();
+    buff.forEach(el => {
+      str += '.' + el;
+      str in res ? res[str]++ : res[str] = 1;
+    })
+  })
+  return res;
 }
 
 module.exports = {
